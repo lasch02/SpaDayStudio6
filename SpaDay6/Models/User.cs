5dd8d721d1a0fbc949d5582b77f02f1161ecb0a2
Linda Schasch <lasch02@sbcglobal.net>
2023-03-28 14:11:14 -0500

    Spa studio - part 1 complete

---
+++ SpaDay6/Models/User.cs
@@ -0,0 +1,18 @@
+﻿namespace SpaDay6.Models
+{
+    public class User
+    {
+        public string Useranme { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string VerifyPassword { get; set; }
+
+        public User(string useranme, string email, string password, string verifyPassword)
+        {
+            Useranme = useranme;
+            Email = email;
+            Password = password;
+            VerifyPassword = verifyPassword;
+        }
+    }
+}
